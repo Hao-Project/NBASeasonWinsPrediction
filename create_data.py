36b54f1a05a90837eb20a167235c6d6a74a0087a
MinervasOwl <MinervasOwl@users.noreply.github.com>
2019-04-22 23:29:43 -0400

    Add create_data.py to create new data columns. The key data columns added include win percentage for team and for opponent, and average point differential for team and for opponent. Auxiciliary data columns include Season/Division/Conference/atHome

---
+++ create_data.py
@@ -0,0 +1,103 @@
+"""This module create the data used for model training and testing."""
+
+import pandas as pd
+import numpy as np
+
+game_data = pd.read_csv("data/input/nba.games.stats.csv")
+
+
+def get_season(str_season):
+    """Get Season"""
+    str_year = str_season[:4]
+    str_month = str_season[5:7]
+    if str_month in ("01", "02", "03", "04", "05", "06"):
+        return int(str_year) - 1
+    if str_month in ("07", "08", "09", "10", "11", "12"):
+        return int(str_year)
+    return None
+
+
+game_data["Season"] = game_data["Date"].apply(get_season)
+
+
+def get_division(team):
+    """Get Division"""
+    if team in ("TOR", "PHI", "BOS", "BRK", "NYK"):
+        division = "Atlantic"
+    elif team in ("MIL", "IND", "DET", "CHI", "CLE"):
+        division = "Central"
+    elif team in ("ORL", "CHO", "MIA", "WAS", "ATL"):
+        division = "Southeast"
+    elif team in ("DEN", "POR", "UTA", "OKC", "MIN"):
+        division = "Northwest"
+    elif team in ("GSW", "LAC", "SAC", "LAL", "PHO"):
+        division = "Pacific"
+    elif team in ("HOU", "SAS", "MEM", "NOP", "DAL"):
+        division = "Southwest"
+    else:
+        division = None
+    return division
+
+
+game_data["Division"] = game_data["Team"].apply(get_division)
+
+
+def get_conference(division):
+    """Get Conference"""
+    if division in ("Atlantic", "Central", "Southeast"):
+        return "East"
+    if division in ("Northwest", "Pacific", "Southwest"):
+        return "West"
+    return None
+
+
+game_data["Conference"] = game_data["Division"].apply(get_conference)
+game_data["inWestConference"] = (game_data["Conference"] == "West")
+
+game_data["atHome"] = (game_data["Home"] == "Home")
+
+# Create Win Percentage Data
+game_data["WinsAdded"] = np.where(game_data["WINorLOSS"] == "W", 1, 0)
+game_data["CumSumWinsAfterGame"] = game_data.groupby(["Season", "Team"])[
+    "WinsAdded"
+].cumsum()
+game_data["CumSumWinsBeforeGame"] = game_data.groupby(["Season", "Team"])[
+    "CumSumWinsAfterGame"
+].shift(1, fill_value=np.nan)
+game_data["WinPctBeforeGame"] = np.where(
+    game_data["Game"] > 1,
+    game_data["CumSumWinsBeforeGame"] / (game_data["Game"] - 1),
+    np.nan,
+)
+game_data = game_data.drop(
+    columns=["WinsAdded", "CumSumWinsAfterGame", "CumSumWinsBeforeGame"]
+)
+
+# Create Average Point Differential Data
+game_data["PointDiff"] = game_data["TeamPoints"] - game_data["OpponentPoints"]
+game_data["CumSumPointDiffAfterGame"] = game_data.groupby(["Season", "Team"])[
+    "PointDiff"
+].cumsum()
+game_data["CumSumPointDiffBeforeGame"] = game_data.groupby(["Season", "Team"])[
+    "CumSumPointDiffAfterGame"
+].shift(1, fill_value=np.nan)
+game_data["AvgPointDiffBeforeGame"] = np.where(
+    game_data["Game"] > 1,
+    game_data["CumSumPointDiffBeforeGame"] / (game_data["Game"] - 1),
+    0,
+)
+game_data = game_data.drop(
+    columns=["CumSumPointDiffAfterGame", "CumSumPointDiffBeforeGame"]
+)
+
+game_data = game_data.merge(
+    game_data[
+        ["Date", "Opponent", "Team", "WinPctBeforeGame", "AvgPointDiffBeforeGame"]
+    ],
+    left_on=["Date", "Team", "Opponent"],
+    right_on=["Date", "Opponent", "Team"],
+    suffixes=("", "_Opponent"),
+)
+game_data = game_data.drop(columns=["Opponent_Opponent", "Team_Opponent"])
+
+game_data.to_csv("data/intermediate/dataForPrediction.csv")
